34894d04b24bbe94b079e1e795da4d1bd4bcfcb1
kutukov2000 <kutukov16.2005@gmail.com>
2023-12-18 13:33:31 +0200

    style: add header margin

--- _header.php
+++ _header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
-        <a href="/" class="d-flex align-items-center link-body-emphasis text-decoration-none">
+        <a href="/" class="d-flex align-items-center link-body-emphasis text-decoration-none mt-2">
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" class="me-2" viewBox="0 0 118 94" role="img">
                 <title>Bootstrap</title>
                 <path fill-rule="evenodd" clip-rule="evenodd"
